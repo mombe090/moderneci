b90f520cdef46b77cd5b2527861361e58ca66b3c
mombe090 <yayamombeya090@gmail.com>
2024-02-09 16:37:27 -0500

    Add Dockerfile and update main.go

--- ci/main.go
+++ ci/main.go
@@ -4,8 +4,9 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -31,10 +32,12 @@ type Response struct {
 	Secrets []Secret `json:"secrets"`
 }
 
-func main() {
-	hcpCloudVaultSecretLoader("ad832508-eed1-4732-9dbc-ea7dd3c363f6", "c37cc2a3-545a-4f30-8578-360572a26a98", "dagger-env")
-	fmt.Println("HCP Cloud Vault secrets loaded ")
+type Project struct {
+	XMLName    xml.Name `xml:"project"`
+	ArtifactId string   `xml:"artifactId"`
+}
 
+func main() {
 	// initialize Dagger client
 	ctx := context.Background()
 	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
@@ -43,11 +46,6 @@ func main() {
 	}
 	defer client.Close()
 
-
-	// set registry password as secret for Dagger pipeline
-	password := client.SetSecret("password", os.Getenv("REGISTRY_PASSWORD"))
-	username := os.Getenv("REGISTRY_USERNAME")
-
 	// create a cache volume for Maven downloads
 	mavenCache := client.CacheVolume("maven-cache")
 
@@ -55,11 +53,25 @@ func main() {
 	source := client.Host().Directory(".", dagger.HostDirectoryOpts{
 		Exclude: []string{"ci", "argocd", "helmChart", "img"},
 	})
+	// load HCP Cloud Vault secrets
+	hcpCloudVaultSecretLoader("ad832508-eed1-4732-9dbc-ea7dd3c363f6", "c37cc2a3-545a-4f30-8578-360572a26a98", "dagger-env")
 
-	
-	// use maven:3.9 container
-	// mount cache and source code volumes
-	// set working directory
+	// build the java application
+	build := appBuilder(client, mavenCache, source)
+
+	// set registry password as secret for Dagger pipeline
+	password := client.SetSecret("password", os.Getenv("REGISTRY_PASSWORD"))
+	username := os.Getenv("REGISTRY_USERNAME")
+
+	// publish image to registry
+	address := publishImageToRegistry(ctx, build, username, password)
+
+	signImage(ctx, client, source, password, username, address)
+
+	scanImageForVulnCheck(ctx, client, address)
+}
+
+func appBuilder(client *dagger.Client, mavenCache *dagger.CacheVolume, source *dagger.Directory) *dagger.Container {
 	app := client.Container().
 		From("maven:3.9-eclipse-temurin-17").
 		WithMountedCache("~/.m2", mavenCache).
@@ -67,68 +79,63 @@ func main() {
 		WithMountedDirectory("/app", source).
 		WithWorkdir("/app")
 
-	// test, scan CVE, build and package application as JAR
 	mavenBuilder := app.WithExec([]string{"mvn", "clean", "install", "sonar:sonar", "-Dsonar.projectKey=app-maven", "-Dsonar.host.url=https://sonarcloud.io", "-Dsonar.token=" + os.Getenv("SONAR_TOKEN")})
 
 	// copy JAR files from builderj
-	deploy := client.Container().
+	return client.Container().
 		From("eclipse-temurin:17-alpine").
 		WithDirectory("/app", mavenBuilder.Directory("./target")).
 		WithEntrypoint([]string{"java", "-jar", "/app/app.jar"})
+}
 
-
-	
-	// publish image to registry
-	address, err := deploy.WithRegistryAuth("docker.io", username, password).
-		Publish(ctx, fmt.Sprintf("%s/myapp:%s", username, getImagesTag()))
+func publishImageToRegistry(ctx context.Context, build *dagger.Container, username string, password *dagger.Secret) string {
+	address, err := build.WithRegistryAuth("docker.io", username, password).
+		Publish(ctx, fmt.Sprintf("%s/%s:%s", username, getAppName(), getImagesTag()))
 	if err != nil {
 		panic(err)
-	} 
-	
+	}
+
 	// print image address
 	fmt.Println("Image published at:", address)
-
-	client.Container().
-		From("anchore/grype:latest").
-		WithExec([]string{address, "--fail-on", "critical"}).
-		Stdout(ctx)
-
-	signImage(ctx, client, source, password, username, address)
-	
+	return address
 }
 
 func signImage(ctx context.Context, client *dagger.Client, source *dagger.Directory, password *dagger.Secret, username string, address string) {
 	cosignKey := client.SetSecret("cosign-key", os.Getenv("COSIGN_KEY"))
 	cosignKeyPassword := client.SetSecret("cosign-key-password", os.Getenv("COSIGN_PASSWORD"))
 
-	cosignContainer := client.Container().
-		From("golang:1.21.6").
-		WithDirectory("/app", source).
-		WithMountedCache("/go/pkg/mod", client.CacheVolume("go-mod-121")).
-		WithEnvVariable("GOMODCACHE", "/go/pkg/mod").
-		WithMountedCache("/go/build-cache", client.CacheVolume("go-build-121")).
-		WithEnvVariable("GOCACHE", "/go/build-cache")
-
-	cosignContainer.
-		WithSecretVariable("COSIGN_PASSWORD", cosignKeyPassword).
+	keyGenContainer := client.Container().
+		From("alpine:3.19.1").
 		WithSecretVariable("COSIGN_KEY", cosignKey).
+		WithWorkdir("/key").
+		WithExec([]string{"sh", "-c", "echo $COSIGN_KEY | base64 -d > cosign.key"})
+
+	_, err := client.Container().
+		From("bitnami/cosign:2.2.3").
+		WithSecretVariable("COSIGN_PASSWORD", cosignKeyPassword).
 		WithSecretVariable("REGISTRY_PASSWORD", password).
-		WithExec([]string{"sh", "-c", fmt.Sprintf(`
-			export GPG_TTY=$(tty)
-			git clone https://github.com/sigstore/cosign.git
-			cd cosign
-			echo $COSIGN_KEY | base64 -d > cosign.key
-			go install ./cmd/cosign
-			echo $REGISTRY_PASSWORD | $(go env GOPATH)/bin/cosign login --username %s --password-stdin docker.io
-			$(go env GOPATH)/bin/cosign sign --key cosign.key %s -y
-			exit_status=$?
-			if [ $exit_status -eq 0 ]; then
-				echo "Command succeeded"
-			else
-				echo "Command failed with exit status $exit_status"
-			fi
-			`, username, address)}).
-		Stderr(ctx)
+		WithDirectory("/app", keyGenContainer.Directory("/key")).
+		WithExec([]string{"login", "docker.io", "-u", username, "--p", os.Getenv("REGISTRY_PASSWORD")}).
+		WithExec([]string{"sign", "--key", "/app/cosign.key", address, "-y"}).
+		Stdout(ctx)
+
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func scanImageForVulnCheck(ctx context.Context, client *dagger.Client, address string) {
+	out, err := client.Container().
+		From("anchore/grype:latest").
+		WithExec([]string{address, "--fail-on", "critical"}).
+		Stdout(ctx)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(out)
 }
 
 func hcpCloudVaultSecretLoader(hcpOrgID string, hcpProjID string, appName string) {
@@ -147,7 +154,7 @@ func hcpCloudVaultSecretLoader(hcpOrgID string, hcpProjID string, appName string
 	}
 	defer resp.Body.Close()
 
-	body, _ := ioutil.ReadAll(resp.Body)
+	body, _ := io.ReadAll(resp.Body)
 	tokenResponse := TokenResponse{}
 	json.Unmarshal(body, &tokenResponse)
 
@@ -164,7 +171,7 @@ func hcpCloudVaultSecretLoader(hcpOrgID string, hcpProjID string, appName string
 	}
 	defer resp.Body.Close()
 
-	body, _ = ioutil.ReadAll(resp.Body)
+	body, _ = io.ReadAll(resp.Body)
 
 	var response Response
 	err = json.Unmarshal(body, &response)
@@ -178,6 +185,8 @@ func hcpCloudVaultSecretLoader(hcpOrgID string, hcpProjID string, appName string
 			log.Fatalf("Failed to set environment variable: %v", err)
 		}
 	}
+
+	fmt.Println("HCP Cloud Vault secrets loaded ")
 }
 
 func getImagesTag() string {
@@ -195,4 +204,20 @@ func getImagesTag() string {
 	imageTag := fmt.Sprintf("%s_%s", branch, timestamp)
 
 	return imageTag
+}
+
+func getAppName() string {
+	xmlFile, err := os.Open("pom.xml")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer xmlFile.Close()
+
+	byteValue, _ := io.ReadAll(xmlFile)
+
+	var project Project
+	xml.Unmarshal(byteValue, &project)
+
+	fmt.Println("Application Name is : " + project.ArtifactId)
+	return project.ArtifactId
 }
